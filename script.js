f767cb5a68f57cbd1a4b08ad36e7b79ea33730bf
Ilim001 <ilim.iskenderov@gmail.com>
2023-04-23 12:34:16 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// const arr =  [1,2,3,4,5]
+// function arraySum(array) {
+//     const sum = 0;
+    
+// for (let i = 0; i < array.length; i++) {
+//     sum += arr[i]
+//     }
+// console.log(sum);
+// }
+
+// arraySum(arr)
+
+// 2
+// const arr = ['a','b','s','a','w','b','s']
+// const arr_2 = arr.filter((item, index) => {
+//     return arr.indexOf(item) !== index
+// });
+// console.log(arr_2);
+
+
+// 3
+// const things = [10,5,2,3,1]
+// things.sort(function(a,b) {
+//     return a - b;
+// });
+// console.log(things);
+
+// 4
+// const words = [
+//     {name: 'John', age: 25, },
+//     {name: 'Sarah', age: 30, },
+//     {name: 'Atay', age: 55},
+
+// ]
+
+// const result = words.filter(word => word.length > 0);
+// console.log(words);
+
+// 6
+// const arr1 = [' палиндром','apple','www']
+// console.log(arr1);
+
+// const reversed = arr1.reverse();
+// console.log('reversed:', reversed);
+
